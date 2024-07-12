1ea1206511b62aa3e326bf61651d7f365a0b5b3f
GastonArthur <gastonarthur14@gmail.com>
2024-07-11 23:20:47 -0300

    database arreglado

--- public/src/database.js
+++ public/src/database.js
@@ -2,16 +2,23 @@ const mysql = require("promise-mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.host,
-    database:process.env.database,
-    user:process.env.user,
-    password:process.env.password 
+    database: process.env.database,
+    user: process.env.user,
+    password: process.env.password 
+});
 
-})
-
-const getConnection = async ()=> await connection;
+const getConnection = async () => {
+    try {
+        const connection = await pool.getConnection();
+        return connection;
+    } catch (error) {
+        console.error('Error al conectar con la base de datos:', error);
+        throw error; // Propagar el error para manejarlo en el código que llama a getConnection()
+    }
+};
 
 module.exports = {
     getConnection
-}
+};
