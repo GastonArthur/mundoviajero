dedb29698fb99aae6a19290acef96fbb6850b5f9
GastonArthur <gastonarthur14@gmail.com>
2024-07-13 22:26:16 -0300

    cambio de puerto en database.js

--- src/database.js
+++ src/database.js
@@ -4,14 +4,14 @@ dotenv.config();
 
 const connection = mysql.createConnection({
     host: process.env.host,
-    database:process.env.database,
-    user:process.env.user,
-    password:process.env.password 
+    port: process.env.port || 3306,
+    database: process.env.database,
+    user: process.env.user,
+    password: process.env.password 
+});
 
-})
-
-const getConnection = async ()=> await connection;
+const getConnection = async () => await connection;
 
 module.exports = {
     getConnection
-}
+};
