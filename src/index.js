5c0e232785c1f30db78770d9956fc460bd1104db
GastonArthur <gastonarthur14@gmail.com>
2024-07-14 00:00:22 -0300

    cambio index.html

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); // Servir archivos estáticos
+app.use(express.static(path.join(__dirname, '..', 'public'))); // Servir archivos estáticos
 
 const secretKey = process.env.SECRET_KEY || 'codoacodo'; // 
 
