9349bb08536b831cd1f226c0b67cb91a22acada5
GastonArthur <gastonarthur14@gmail.com>
2024-07-17 22:30:55 -0300

    ultimo cambio

--- src/database.js
+++ src/database.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.mysql_host,
-    user: process.env.mysql_user,
-    password: process.env.mysql_password,
-    database: process.env.mysql_database,
+    host: bmjy4kq6uat1llvnlhr1-mysql.services.clever-cloud.com,
+    user: ut6c8s0ulpjtmq1p,
+    password: WfYeRg9v9TjmXtXfjF8J,
+    database: bmjy4kq6uat1llvnlhr1,
     waitForConnections: true,
     connectionLimit: 5,
     queueLimit: 0
